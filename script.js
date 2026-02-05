d2b5daf2bdc2af0d95cf8969354208fd5348d416
hamza aballagh <hamzaaballagh941@gmail.com>
2026-02-05 15:26:23 +0100

    travailler sur le formule add les inputs

--- script.js
+++
@@ -1,9 +0,0 @@
-const addBtn = document.querySelector(".add-btn");
-const closeBtn = document.querySelector(".close-window");
-
-addBtn.addEventListener("click", () => {
-  document.querySelector(".overlay").classList.remove("hide");
-});
-closeBtn.addEventListener("click", () => {
-  document.querySelector(".overlay").classList.add("hide");
-});
